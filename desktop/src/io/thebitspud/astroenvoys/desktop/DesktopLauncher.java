861c2c848878894ba0bb75ea0e012a927fd3c7d5
James Zhong <thebitspud@gmail.com>
2020-03-14 12:40:53 -0700

    Created LibGDX project

---
+++ desktop/src/io/thebitspud/astroenvoys/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package io.thebitspud.astroenvoys.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import io.thebitspud.astroenvoys.AstroEnvoys;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new AstroEnvoys(), config);
+	}
+}
